7254718f8be2f0aa659cae9d67003e1fe248028d
Florent TOMI <florent.tomi@outlook.com>
2026-08-09 21:03:50 +0200

    Update snapshots.nix

--- hosts/base/snapshots.nix
+++ hosts/base/snapshots.nix
@@ -2,23 +2,22 @@
 {
   services.btrbk.instances.local = {
     onCalendar = "daily";
-    snapshotOnly = true;
     settings = {
       snapshot_preserve_min = "1d";
       snapshot_preserve = "5d 3w";
       volume."/" = {
         subvolume."." = {
-          snapshot_create = "always";
+          snapshot_create = "onchange";
         };
       };
       volume."/home" = {
         subvolume."." = {
-          snapshot_create = "always";
+          snapshot_create = "onchange";
         };
       };
       volume."/var/log" = {
         subvolume."." = {
-          snapshot_create = "always";
+          snapshot_create = "onchange";
         };
       };
     };
