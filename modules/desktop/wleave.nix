84d88cfb062ac7ed4b4bee9f1c63b308fc55f359
Florent TOMI <florent.tomi@outlook.com>
2026-08-24 17:03:54 +0200

    wleave icons

--- modules/desktop/wleave.nix
+++ modules/desktop/wleave.nix
@@ -1,5 +1,7 @@
 {
-  homeManager.modules.wleave = {
+  homeManager.modules.wleave = 
+  {pkgs, ...}:
+  {
     programs.wleave = {
       enable = true;
       settings = {
@@ -9,18 +11,21 @@
             action = "systemctl poweroff";
             text = "Shutdown";
             keybind = "s";
+            icon = "${pkgs.wleave}/share/wleave/icons/shutdown.svg";
           }
           {
             label = "reboot";
             action = "systemctl reboot";
             text = "Reboot";
             keybind = "r";
+            icon = "${pkgs.wleave}/share/wleave/icons/reboot.svg";
           }
           {
             label = "lock";
             action = "hyprlock";
             text = "Lock";
             keybind = "l";
+            icon = "${pkgs.wleave}/share/wleave/icons/lock.svg";
           }
         ];
       };
